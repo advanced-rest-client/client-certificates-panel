0bb2c3e7063391371ee9f3dee184739e75d74432
Pawel Psztyc <jarrodek@gmail.com>
2019-12-31 00:02:55 -0800

    chore: adding "module" to package.json file

---
+++ index.js
@@ -0,0 +1,3 @@
+export { ClientCertificatesPanel } from './src/ClientCertificatesPanel.js';
+export { CertificateDetails } from './src/CertificateDetails.js';
+export { CertificateImport } from './src/CertificateImport.js';
